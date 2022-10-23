de4506a7aa560cf2461276edd58f9801bb5f3a94
Alejandro Casamayor <alex.casamayor.munoz@gmail.com>
2022-10-23 20:23:10 +0200

    Se añaden bien, falta conf button eliminar

--- js/model.js
+++ js/model.js
@@ -1,6 +1,6 @@
 var listaTareas = new Array();
 listaTareas = [
-    {'idTarea':0, 'titulo':'Estudiar Javascript', 'prioridad':'urgente'},
-    {'idTarea':1, 'titulo':'Dormir', 'prioridad':'diaria'},
-    {'idTarea':2, 'titulo':'Salir a comer', 'prioridad':'mensual'}
+    {'idTarea':0, 'titulo':'Estudiar Javascript', 'prioridad':'Urgente'},
+    {'idTarea':1, 'titulo':'Dormir', 'prioridad':'Diaria'},
+    {'idTarea':2, 'titulo':'Salir a comer', 'prioridad':'Mensual'}
 ]
--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,60 @@
 const inputTarea = document.getElementById('inputTarea')
 const btnGuardar = document.getElementById('btnGuardar')
 const SectionTareas = document.getElementById('SectionTareas')
+const selectPrioridad = document.getElementById('selectPrioridad')
 
-
-function addTarea (arrTareas,pTitulo,pPrioridad){
+function addTarea (arrTareas,pTitulo){
     const cont = arrTareas.length;
     let tareaCreada= 
         {
         'idTarea': cont,
         'titulo': pTitulo,
-        'prioridad': pPrioridad
+        'prioridad': selectPrioridad.value
     }
     arrTareas.push(tareaCreada)
 }
 
 function pintarTareas () {
+    SectionTareas.innerHTML= "";
     const tareas = JSON.parse(localStorage.getItem('tarea'))
-    console.log(tareas)
     for (let tarea of tareas) {
         const articleTarea = document.createElement('article')
         const ptitulo = document.createElement('p')
         const deleteButton = document.createElement('button')
+
         deleteButton.innerText= 'ELIMINAR'
         ptitulo.innerText = tarea.titulo
-        
+
         articleTarea.append(ptitulo,deleteButton)
+        articleTarea.dataset.prioridad= tarea.prioridad;
+
+        if (articleTarea.dataset.prioridad == 'Urgente') {
+            articleTarea.style.backgroundColor = '#FAA0A0'
+        }
+        if (articleTarea.dataset.prioridad == 'Diaria') {
+            articleTarea.style.backgroundColor = 'lightblue'
+        }
+        if (articleTarea.dataset.prioridad == 'Mensual') {
+            articleTarea.style.backgroundColor = 'lightgreen'
+        }
+
         SectionTareas.append(articleTarea)
+        
     }
 }
 
 btnGuardar.addEventListener('click',() => {
-    addTarea(listaTareas,inputTarea.value,'urgente')
+    addTarea(listaTareas,inputTarea.value)
     const strTexto = JSON.stringify(listaTareas)
     localStorage.setItem('tarea',strTexto)
     pintarTareas()
 })
+
+
+/* FILTROS?
+if (selectPrioridad.value==='Urgente'){
+            articleTarea.dataset.urgente
+        } else {
+            break;
+        }
+ */
