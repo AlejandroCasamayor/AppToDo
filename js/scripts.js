46c19ca7c6667b175392f3082680fa543d5a9fa5
Alejandro Casamayor <alex.casamayor.munoz@gmail.com>
2022-10-24 20:55:44 +0200

    Entregable

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ const inputTarea = document.getElementById('inputTarea')
 const btnGuardar = document.getElementById('btnGuardar')
 const SectionTareas = document.getElementById('SectionTareas')
 const selectPrioridad = document.getElementById('selectPrioridad')
+const prioridadTarea = document.getElementById('prioridadTarea')
 
 function addTarea (arrTareas,pTitulo){
-    const cont = arrTareas.length;
+    let cont = arrTareas.length;
     let tareaCreada= 
         {
         'idTarea': cont,
@@ -18,13 +19,17 @@ function pintarTareas () {
     SectionTareas.innerHTML= "";
     const tareas = JSON.parse(localStorage.getItem('tarea'))
     for (let tarea of tareas) {
-        console.log(cont)
         const articleTarea = document.createElement('article')
         const ptitulo = document.createElement('p')
         const deleteButton = document.createElement('button')
-
         deleteButton.innerText= 'ELIMINAR'
-        deleteButton.dataset.idtarea
+        deleteButton.className= 'btnBorrar'
+        deleteButton.addEventListener('click',(event)=>{
+            event.target.parentNode.remove();
+            tareas.splice(tarea.idTarea,1)
+            console.log(tareas) /* consigo eliminarlo del array pero no se quita de localstorage, por lo que al volver a pintar se pinta lo almacenado en el localstorage
+            supongo que lo he hecho mal desde el principio */
+        })
         ptitulo.innerText = tarea.titulo
 
         articleTarea.append(ptitulo,deleteButton)
@@ -39,12 +44,9 @@ function pintarTareas () {
         if (articleTarea.dataset.prioridad == 'Mensual') {
             articleTarea.style.backgroundColor = 'lightgreen'
         }
-
         SectionTareas.append(articleTarea)
-        
-    }
+    }    
 }
-
 btnGuardar.addEventListener('click',() => {
     addTarea(listaTareas,inputTarea.value)
     const strTexto = JSON.stringify(listaTareas)
@@ -52,11 +54,33 @@ btnGuardar.addEventListener('click',() => {
     pintarTareas()
 })
 
+function filtrarPrioridad() {
+    if (prioridadTarea.value =='Urgente'){
+        pintarTareas()
+    }
+}
+prioridadTarea.addEventListener('change',() =>{
+    filtrarPrioridad()
+}
+)
 
-/* FILTROS?
-if (selectPrioridad.value==='Urgente'){
-            articleTarea.dataset.urgente
-        } else {
-            break;
+
+
+/* deleteButton.addEventListener('click',()=>{
+    tareas.splice(tarea.idTarea,1) 
+    console.log(tarea.idTarea)// Devuelve la posision del array
+    console.log(typeof tarea.idTarea) // es numero
+    pintarTareas()
+    event.target.parentNode.remove(); // Elimina el article pero no el array
+}) */
+
+/* 
+        function filtrarPrioridad() {
+            if (prioridadTarea.value =='Urgente'){
+                console.log('hola')
+            }
         }
- */
+        prioridadTarea.addEventListener('change',() =>{
+            filtrarPrioridad()
+        }
+        ) */
